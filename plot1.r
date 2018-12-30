d63f28dd4f936ec64dab7bdd0b583d0550b0471f
Xingmin Zhang <kingmanzhang@gmail.com>
2018-12-29 23:56:28 -0500

    finish week1

---
+++ plot1.r
@@ -0,0 +1,43 @@
+## set working directory
+wd = "~/git/ExData_Plotting1"
+if (dir.exists(wd)) {
+    setwd(wd)
+} else {
+    setwd("~/")
+}
+
+filenamezip = "household_power_consumption.zip"
+filename = "household_power_consumption.txt"
+## download data
+if (!file.exists(filename)) {
+    fileurl = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(fileurl, destfile = filenamezip)
+    unzip(filenamezip)
+}
+
+## read data
+data <- read.table(file = filename, sep = ";", header = TRUE, colClasses = "character", na.strings = "?")
+
+desiredDateIndex <- grep("^[0]?[12]/[0]?2/2007", data$Date)
+subset <- data[desiredDateIndex,]
+
+require(dplyr)
+require(lubridate)
+
+subset <- subset %>% 
+    mutate(DateTime = dmy_hms(paste(subset$Date, subset$Time, sep = " "), tz = "Europe/Paris"), 
+            Global_active_power = as.numeric(Global_active_power),
+            Global_reactive_power = as.numeric(Global_reactive_power),
+            Voltage = as.numeric(Voltage),
+            Global_intensity = as.numeric(Global_intensity),
+            Sub_metering_1 = as.numeric(Sub_metering_1),
+            Sub_metering_2 = as.numeric(Sub_metering_2),
+            Sub_metering_3 = as.numeric(Sub_metering_3)) %>%
+    select(-c(Date, Time)) %>% 
+    select(DateTime, everything())
+
+##plot1
+hist(subset$Global_active_power, col = "red", 
+     main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.copy(png, filename = "plot1.png")
+dev.off()
---
+++ plot4.r
@@ -0,0 +1,55 @@
+## set working directory
+wd = "~/git/ExData_Plotting1"
+if (dir.exists(wd)) {
+    setwd(wd)
+} else {
+    setwd("~/")
+}
+
+filenamezip = "household_power_consumption.zip"
+filename = "household_power_consumption.txt"
+## download data
+if (!file.exists(filename)) {
+    fileurl = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(fileurl, destfile = filenamezip)
+    unzip(filenamezip)
+}
+
+## read data
+data <- read.table(file = filename, sep = ";", header = TRUE, colClasses = "character", na.strings = "?")
+
+desiredDateIndex <- grep("^[0]?[12]/[0]?2/2007", data$Date)
+subset <- data[desiredDateIndex,]
+
+require(dplyr)
+require(lubridate)
+
+subset <- subset %>% 
+    mutate(DateTime = dmy_hms(paste(subset$Date, subset$Time, sep = " "), tz = "Europe/Paris"), 
+           Global_active_power = as.numeric(Global_active_power),
+           Global_reactive_power = as.numeric(Global_reactive_power),
+           Voltage = as.numeric(Voltage),
+           Global_intensity = as.numeric(Global_intensity),
+           Sub_metering_1 = as.numeric(Sub_metering_1),
+           Sub_metering_2 = as.numeric(Sub_metering_2),
+           Sub_metering_3 = as.numeric(Sub_metering_3)) %>%
+    select(-c(Date, Time)) %>% 
+    select(DateTime, everything())
+
+#plot4
+par(mfrow = c(2, 2))
+with(subset, {
+    plot(Global_active_power ~ DateTime, type = "l", xlab = "", ylab = "Global Active Power")
+    plot(Voltage ~ DateTime, type = "l", xlab = "datetime", ylab = "Votage")
+    plot(subset$Sub_metering_1 ~ subset$DateTime, type = "l", 
+         xlab = "", ylab = "Energy sub metering")
+    lines(subset$Sub_metering_2 ~ subset$DateTime, type = "l", col = "red")
+    lines(subset$Sub_metering_3 ~ subset$DateTime, type = "l", col = "blue")
+    legend("topright", lty = 1, col = c("gray", "red", "blue"), 
+           legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+           y.intersp = 0.25, bty = "n", cex = 0.75, inset = c(-0.1, -0.1))
+    plot(Global_reactive_power ~ DateTime, type = "l", xlab = "datetime")
+    
+})
+dev.copy(png, filename = "plot4.png")
+dev.off()
